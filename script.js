56cbe9a9df0fecb12fac1373a056870cf0f32c1c
akowala <110314169+akowala@users.noreply.github.com>
2022-09-19 02:58:42 -0500

    working on local storage in script. assigned ids in html

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ $("#currentDay").text(currentDay);
 
 
 
+// local storage mockup
+//assign ids to containers
+
+localStorage.getItem("9");
+localStorage.getItem("10");
+localStorage.getItem("11");
+localStorage.getItem("12");
+localStorage.getItem("13");
+localStorage.getItem("14");
+localStorage.getItem("15");
+localStorage.getItem("16");
+localStorage.getItem("17");
