aa2bbf489430e501a53859cc80b71c990049f05f
akowala <110314169+akowala@users.noreply.github.com>
2022-09-19 22:39:22 -0500

    finally figured out local storage. added elements to the code and the correct ids to match

--- script.js
+++ script.js
@@ -6,25 +6,42 @@ $("#currentDay").text(currentDay);
 
 
 
+
+
 // local storage mockup
 //assign ids to containers
+
+
+//localstorage function has errors
+
 $(function () {      
     $(".saveBtn").on("click", function () {
+        var time = $(this).parent().attr("id");
+        var text = $(this).siblings(".description").val();
 
-        var time = $(this).("id");
-        var hour = $(this).(".description");
-        localStorage.setItem(time, hour);
+        localStorage.setItem(time, text);
     })
 
 })
 
 
-$localStorage.getItem("9");
-$localStorage.getItem("10");
-$localStorage.getItem("11");
-$localStorage.getItem("12");
-$localStorage.getItem("13");
-$localStorage.getItem("14");
-$localStorage.getItem("15");
-$localStorage.getItem("16");
-$localStorage.getItem("17");
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+
+//mockup local storage jquery
+// $localStorage.getItem("10");
+// $localStorage.getItem("11");
+// $localStorage.getItem("12");
+// $localStorage.getItem("13");
+// $localStorage.getItem("14");
+// $localStorage.getItem("15");
+// $localStorage.getItem("16");
+// $localStorage.getItem("17");
