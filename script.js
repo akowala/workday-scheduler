3ae647eed17c1e7b6be2d58f67a3e63f489b258b
akowala <110314169+akowala@users.noreply.github.com>
2022-09-19 21:57:26 -0500

    working on local storage on javascript using jquery

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  //current day ID is on html
 
-var currentDay = moment().format("ddd, MMM Do YYYY, h:mm:ss a");
+var currentDay = moment().format("ddd, MMM Do YYYY, h:mm a");
 $("#currentDay").text(currentDay);
 
 
@@ -8,13 +8,23 @@ $("#currentDay").text(currentDay);
 
 // local storage mockup
 //assign ids to containers
+$(function () {      
+    $(".saveBtn").on("click", function () {
 
-localStorage.getItem("9");
-localStorage.getItem("10");
-localStorage.getItem("11");
-localStorage.getItem("12");
-localStorage.getItem("13");
-localStorage.getItem("14");
-localStorage.getItem("15");
-localStorage.getItem("16");
-localStorage.getItem("17");
+        var time = $(this).("id");
+        var hour = $(this).(".description");
+        localStorage.setItem(time, hour);
+    })
+
+})
+
+
+$localStorage.getItem("9");
+$localStorage.getItem("10");
+$localStorage.getItem("11");
+$localStorage.getItem("12");
+$localStorage.getItem("13");
+$localStorage.getItem("14");
+$localStorage.getItem("15");
+$localStorage.getItem("16");
+$localStorage.getItem("17");
