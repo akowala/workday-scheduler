a55c189b69ec26bc46df067133678e59557f2c95
akowala <110314169+akowala@users.noreply.github.com>
2022-09-19 23:27:36 -0500

    added folders for organization. completed README. could not figure out how to add colors for past/future/present

--- script/script.js
+++ script/script.js
@@ -1,22 +1,17 @@
- //current day ID is on html
-
+//  moment.js is added and formatted to display the date in current time
 var currentDay = moment().format("ddd, MMM Do YYYY, h:mm a");
 $("#currentDay").text(currentDay);
 
 
 
 
-
-
-// local storage mockup
-//assign ids to containers
-
-
-//localstorage function has errors
-
+// local storage is added with jQuery functions
 $(function () {      
+    // when the save button is clicked on the live page, it is stored to local storage and not erased after refreshing the page
     $(".saveBtn").on("click", function () {
+        // jQuery parent selector is attached to the id tag in html
         var time = $(this).parent().attr("id");
+        // siblings selector added to obtain text in local storage
         var text = $(this).siblings(".description").val();
 
         localStorage.setItem(time, text);
@@ -24,7 +19,7 @@ $(function () {
 
 })
 
-
+// getitem method will return the value from each column
 $("#9 .description").val(localStorage.getItem("9"));
 $("#10 .description").val(localStorage.getItem("10"));
 $("#11 .description").val(localStorage.getItem("11"));
@@ -36,12 +31,3 @@ $("#16 .description").val(localStorage.getItem("16"));
 $("#17 .description").val(localStorage.getItem("17"));
 
 
-//mockup local storage jquery
-// $localStorage.getItem("10");
-// $localStorage.getItem("11");
-// $localStorage.getItem("12");
-// $localStorage.getItem("13");
-// $localStorage.getItem("14");
-// $localStorage.getItem("15");
-// $localStorage.getItem("16");
-// $localStorage.getItem("17");
